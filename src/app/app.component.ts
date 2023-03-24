18df048f8f70c887eceff453c5cdec8358799745
Mariami985 <mariami.bregvadze17b@geolab.edu.ge>
2023-03-25 01:26:27 +0400

    add fakeApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './service/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'products-app';
+
+  prodacts!: any[];
+
+  constructor(private productsService: HttpService) { }
+
+  ngOnInit () {
+           this.productsService.getProdact().subscribe(response => {
+            this.prodacts = response;
+
+                  console.log(response)
+           })
+  }
 }
